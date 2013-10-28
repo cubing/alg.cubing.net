b637438546f5e7c25d0bd1a8f6b17ca0bc8b3d51
Lucas Garron <lucasg@gmx.de>
2013-10-28 07:43:43 -0700

    Display title in page title.

--- inc/alg.js
+++ inc/alg.js
@@ -12,12 +12,13 @@ algxApp.config(['$locationProvider',
 }]);
 
 
-algxApp.filter('abbreviate', function() {
-  return function(input) {
+algxApp.filter('title', function() {
+  return function(input, title) {
+    var prefix = title ? title + " | " : "";
     if (input.length > 20) {
-       return input.slice(0, 20) + (input.slice(20, 30) + " ").split(" ")[0] + "...";
+       return prefix + input.slice(0, 20) + (input.slice(20, 30) + " ").split(" ")[0] + "...";
     }
-    return input;
+    return prefix + input;
   };
 });
 
