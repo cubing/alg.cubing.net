3d5461d44cd65a96b4123a019aaf48f8596ba44f
Robin Carlier <concour.ed@gmail.com>
2014-08-25 15:11:12 +0200

    Finished wrapping API into class, Security done, Link generation done.

--- alg.cubing.net.api.js
+++ alg.cubing.net.api.js
@@ -1,29 +1,26 @@
-// Define the site base here
-var site_base = "http://alg.cubing.net";
-
-// Preset list for easier use of this API
-// Using var instead of const to provide some backwards compatibilty with old IE 
-
-var PRESET_OLL = {
-	'view' : 'playback',
-	'type' : 'alg',
-	'puzzle' : '3x3x3',
-	'stage' : 'OLL'
-};
-
-var PRESET_PLL = {
-	'view' : 'playback',
-	'type' : 'alg',
-	'puzzle' : '3x3x3',
-	'stage' : 'PLL'
-};
 
 
 // Main class for the API
 //Just call it with the URL of the site;
+// Typical call : var api = new CubingAPI('alg.cubing.net');
 
-var CubingAPI(baseSite){
 
+function CubingAPI(baseSite){
+	// This little custom exception will be usefull
+	function IllegalArgumentException(message) {
+    		this.message = message;
+	}
+
+	//Reimplement in_array : We will need it;
+	// This is copy of Jquery in_array function;
+	function in_array(needle, haystack) {
+   	 	var length = haystack.length;
+    		for(var i = 0; i < length; i++) {
+        		if(haystack[i] == needle) return true;
+		}
+ 		return false;
+	}
+	
 	this.PRESET_OLL = {
 		'view' : 'playback',
 		'type' : 'alg',
@@ -38,7 +35,88 @@ var CubingAPI(baseSite){
 		'stage' : 'PLL'
 	};
 	
+
+
 	this.baseSite = baseSite;
+
+
+	// The allowed GET options in a list. Any value out of this list passed to generate_link will throw and exception
+	this.allowedOptions = ['alg', 
+		'setup',
+		'puzzle',
+		'type',
+		'scheme',
+		'stage',
+		'title',
+		'view'
+	];
+
+
+	/* This big object contains restriction for certains values.
+	 * To add a restriction for an option, add this option name as key
+	 * Bind the option name to an array of two vars : The type of restriction (can be regex or set);
+	 * The second var is the set or the regex.
+	 *
+	 * Simple example, to allow only "ABC" and "CDE" values for "foo" option, it would be:
+	 * "foo" : ['set', ["ABC", "CDE"]]
+	 *
+	 * To allow regex, this is similar:
+	 * "bar" : ['regex', /^bar$/g]; 
+	 */
+	this.constraint = {
+		'puzzle' : ['set',
+			[
+				'1x1x1',
+				'2x2x2',
+				'3x3x3',
+				'4x4x4',
+				'5x5x5',
+				'6x6x6',
+				'7x7x7',
+				'8x8x8',
+				'9x9x9',
+				'17x17x17'
+			]	
+		], // We could rewrite this one with regex...
+		'stage' : ['set',
+			[
+				'full',
+				'PLL',
+				'OLL',
+				'LL',
+				'F2L',
+				'CLS',
+				'ELS',
+				'L6E',
+				'WV'
+			]
+		],
+		'type' : ['set',
+			[
+				'moves',
+				'reconstruction',
+				'alg',
+				'reconstruction-end-with-setup'
+			]
+		],
+		'scheme' : ['set',
+			[
+				'boy',
+				'japanese',
+				'custom' // Should we allow custom scheme for link generation? it could be useless
+			]
+		],
+		'view' : ['set',
+			[
+				'editor',
+				'playback',
+				'fullscreen'
+			]
+		]
+		// Maybe could we add a char number limit for titles?
+		
+	};
+
 
 	this.escape_alg = function(alg) {
     		if (!alg) {return alg;}
@@ -49,38 +127,11 @@ var CubingAPI(baseSite){
     		return escaped;
 	}
 
-	this.toBeEscaped = ['alg', 'scramble']; // We set here the var that should be escaped with escape_alg;
+	this.toBeEscaped = ['alg', 'setup']; // We set here the var that should be escaped with escape_alg;
 
-	this.generateLink(infos, preset){
-		
-
-	}
-	this.generateOLL(){
-
-	}
-	this.generatePLL(){
-
-	}
-
-	// Encode a query string from datas
-	this.encode_query(data){
-		var ret = [];
-		for(var d in data){
-			if(d in this.toBeEscaped){
-				data[d] = this.escape_alg(data[d]);		
-			}
-			ret.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));	
-		
-		}
-		return ret.join("&");
-	}
-}
-
-
-
-function generateLink(infos, preset){
-	if(infos != undefined){
-		//We handle the presets:
+	this.generate_link = function(infos, preset){
+		//If a preset is passed, we merge it with infos
+		//TODO Create an array containing all the preset and check preset validity
 		if(preset != undefined){
 			for(var key in preset){
 				if(!infos.hasOwnProperty(key)){
@@ -90,26 +141,68 @@ function generateLink(infos, preset){
 				}
 			}
 		}
+		try{
+			var encodedDatas = this.encode_query(infos);
+			return 'http://'+this.baseSite+'/?'+encodedDatas;
+		} catch (e){
+			console.log(e);
+			//In case of exception during query encoding, return false
+			return false;
+		}	
 
+	}
+	this.generate_oll = function(alg, name){
+		infos = {'alg' : alg};
+		if(name != undefined){
+			infos['title'] = name;
+		}
+		return this.generate_link(infos, this.PRESET_OLL);
+	}
+	this.generate_pll = function(alg, name){
+		infos = {'alg' : alg};
+		if(name != undefined){
+			infos['title'] = name;
+		}
+		return this.generate_link(infos, this.PRESET_PLL);
+	}
 
-		var param_list = '/?';
-
-		// We place every type to be escaped here
-		var to_be_escaped = ['alg', 'scramble'];
-
-
-
-		for(var key in infos){
-			if(key == "alg"){
-				param_list += (key+'='+escape_alg(infos[key])+'&');
+	// Encode a query string from datas
+	this.encode_query = function(data){
+		var ret = [];
+		for(var d in data){
+			//We first check if this data is allowed:
+			if(in_array(d, this.allowedOptions)){
+				// Now we compute restrictions to see if the value is good:
+				if(d in this.constraint){ // if the data is restricted
+					switch(this.constraint[d][0]){
+						case 'regex':
+							if(!data[d].match(this.constraint[d][1])){throw new IllegalArgumentException('The given value for ' + d +' doesn\'t respect his restrictions')}
+							break;
+						case 'set':
+							if(!in_array(data[d], this.constraint[d][1])){throw new IllegalArgumentException('The given value for ' + d +' doesn\'t respect his restrictions')}
+							break;
+		
+						default:
+							// Bad use of constraint. Throw an exception to notify it
+							throw{
+								'name':'IllegalModifierException',
+								'message' : 'The modifiers for constraint object can only be "regex" or "set" (given '+this.constraint[d][O]+')'
+							};	
+							break;
+					}
+				}
+				if(in_array(d,this.toBeEscaped)){
+					data[d] = this.escape_alg(data[d]);		
+				}
+				ret.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));	
+		
 			} else {
-				param_list += (key+'='+encodeURIComponent(infos[key])+'&');
+				throw new IllegalArgumentException(d +' isn\'t in the allowed options');
 			}
 		}
-		param_list = param_list.substr(0, param_list.length-1);
-		return site_base+param_list;
-	} else {
-		return site_base;
+		return ret.join("&");
 	}
 }
+
+
 
