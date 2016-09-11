c1a6413205922f8fc65f8e42cc05a0d6fac2b560
Lucas Garron <code@garron.net>
2016-09-11 01:19:37 -0700

    Stub `Twisty` implementation in Typescript. Adds a canvas and control bar.

---
+++ index.ts
@@ -0,0 +1,6 @@
+"use strict";
+
+window.addEventListener("load", function() {
+  let elem = document.querySelector("twisty");
+  new Twisty(elem);
+});
---
+++ twisty/twisty.ts
@@ -0,0 +1,40 @@
+"use strict";
+
+// TODO: learn how to use modules
+
+class Twisty {
+  private canvas: HTMLCanvasElement;
+  private controlBar: TwistyControlBar;
+  constructor(public element: Element) {
+    this.canvas = document.createElement("canvas");
+    this.controlBar = new TwistyControlBar(this);
+
+    this.element.appendChild(this.canvas);
+    this.element.appendChild(this.controlBar.element);
+  }
+
+  // Initialize a Twisty for the given Element unless the elements
+  // `initialization` attribute is set to `custom`.
+  static smartInitialize(elem: Element) {
+    let ini = elem.getAttribute("initialization");
+    if (ini !== "custom") {
+      new Twisty(elem);
+    }
+  }
+}
+
+class TwistyControlBar {
+  public element;
+  constructor(public twisty: Twisty) {
+    this.element = document.createElement("twisty-control-bar");
+  }
+}
+
+window.addEventListener("load", function() {
+  let elems = document.querySelectorAll("twisty");
+  console.log(`Found ${elems.length} twisty elem${elems.length === 1 ? "" : "s"} on page.`)
+
+  for (let i = 0; i < elems.length; i++) {
+    Twisty.smartInitialize(elems[i]);
+  }
+});
