fb23fb9366451c16c553fcc1d246168657ce6732
Lucas Garron <code@garron.net>
2016-11-22 23:21:31 -0800

    Create a button class.

--- twisty/widget.ts
+++ twisty/widget.ts
@@ -3,6 +3,8 @@
 interface Document {
     mozCancelFullScreen: () => void;
     msExitFullscreen: () => void;
+    mozFullScreenElement: HTMLElement
+    msFullscreenElement: HTMLElement
 }
 
 interface Element {
@@ -13,75 +15,97 @@ interface Element {
 namespace Twisty {
 export namespace Widget {
 
+export abstract class Button {
+  public element: HTMLButtonElement;
+  constructor(title: string, initialClass: string) {
+    this.element = document.createElement("button");
+    this.element.title = title;
+    // TODO: Handle updating image based on anim state.
+    this.element.classList.add(initialClass);
+    this.element.addEventListener("click", this.onpress.bind(this));
+  }
+
+  abstract onpress(): void
+}
+
+namespace FullscreenAPI {
+  export function element() {
+    return document.fullscreenElement ||
+           document.webkitFullscreenElement ||
+           document.mozFullScreenElement ||
+           document.msFullscreenElement ||
+           document.webkitFullscreenElement;
+  }
+  export function request(element: Element) {
+    var requestFullscreen = element.requestFullscreen ||
+                            element.mozRequestFullScreen ||
+                            element.msRequestFullscreen ||
+                            element.webkitRequestFullscreen;
+    requestFullscreen.call(element);
+  }
+  export function exit() {
+    var exitFullscreen = document.exitFullscreen ||
+                         document.mozCancelFullScreen ||
+                         document.msExitFullscreen ||
+                         document.webkitExitFullscreen;
+    exitFullscreen.call(document);
+  }
+}
+
+export module Button {
+
+  export class Fullscreen extends Button {
+    constructor(private fullscreenElement: Element) {
+      super("Full Screen", "fullscreen");
+    }
+
+    onpress(): void {
+      if (FullscreenAPI.element() === this.fullscreenElement) {
+        FullscreenAPI.exit();
+      } else {
+        FullscreenAPI.request(this.fullscreenElement);
+      }
+    }
+  }
+
+  export class SkipToStart extends Button {
+    constructor(private anim: Anim.Controller) {
+      super("Skip To Start", "skip-to-start"); }
+    onpress(): void { this.anim.skipToStart(); }
+  }
+  export class SkipToEnd extends Button {
+    constructor(private anim: Anim.Controller) {
+      super("Skip To End", "skip-to-end"); }
+    onpress(): void { this.anim.skipToEnd(); }
+  }
+  export class PlayPause extends Button {
+    constructor(private anim: Anim.Controller) {
+      super("Play", "play"); }
+    onpress(): void { this.anim.togglePausePlayForward(); }
+  }
+  export class StepForward extends Button {
+    constructor(private anim: Anim.Controller) {
+      super("Step forward", "step-forward"); }
+    onpress(): void { this.anim.stepForward(); }
+  }
+  export class StepBackward extends Button {
+    constructor(private anim: Anim.Controller) {
+      super("Step backward", "step-backward"); }
+    onpress(): void { this.anim.stepBackward(); }
+  }
+}
+
 export class ControlBar {
-  private isFullscreen: boolean = false;
-  private buttonElems: {[key: string]:Element}; // TODO: Use ES6 Map.
   public element: HTMLElement;
   constructor(private anim: Anim.Controller, private twistyElement: Element) {
     this.element = document.createElement("twisty-control-bar");
 
-    // TODO: Use SVGs or a web font for element-relative sizing.
-    const buttons = [{
-        title: "Cycle display mode",
-        id: "fullscreen",
-        initialClass: "fullscreen",
-        fn: this.toggleFullscreen.bind(this)
-      }, {
-        title: "Skip to start",
-        id: "skip-to-start",
-        initialClass: "skip-to-start",
-        fn: anim.skipToStart.bind(anim)
-      }, {
-        title: "Step back",
-        id: "step-backward",
-        initialClass: "step-backward",
-        fn: anim.stepBackward.bind(anim)
-      }, {
-        title: "Play",
-        id: "play",
-        initialClass: "play",
-        fn: anim.togglePausePlayForward.bind(anim) // TODO: Toggle between play and pause icon.
-      }, {
-        title: "Step forward",
-        id: "step-forward",
-        initialClass: "step-forward",
-        fn: anim.stepForward.bind(anim)
-      }, {
-        title: "Skip to end",
-        id: "skip-to-end",
-        initialClass: "skip-to-end",
-        fn: anim.skipToEnd.bind(anim)
-      }];
-
-    this.buttonElems = {};
-
-    for (let i = 0; i < buttons.length; i++) {
-      const button = document.createElement("button");
-      button.title = buttons[i].title;
-      // TODO: Handle updating image based on anim state.
-      button.classList.add(buttons[i].initialClass);
-      button.addEventListener("click", buttons[i].fn);
-
-      this.buttonElems[buttons[i].id] = button;
-      this.element.appendChild(button);
-    }
-  }
-
-  toggleFullscreen() {
-    if (this.isFullscreen) {
-      var exitFullscreen = document.exitFullscreen ||
-                           document.mozCancelFullScreen ||
-                           document.msExitFullscreen ||
-                           document.webkitExitFullscreen;
-      exitFullscreen.call(document);
-    } else {
-      var requestFullscreen = this.twistyElement.requestFullscreen ||
-                              this.twistyElement.mozRequestFullScreen ||
-                              this.twistyElement.msRequestFullscreen ||
-                              this.twistyElement.webkitRequestFullscreen;
-      requestFullscreen.call(this.twistyElement);
-    }
-    this.isFullscreen = !this.isFullscreen;
+    this.element.appendChild((new Button.Fullscreen(twistyElement)).element);
+    this.element.appendChild((new Button.SkipToStart(anim)).element);
+    this.element.appendChild((new Button.StepBackward(anim)).element);
+    this.element.appendChild((new Button.PlayPause(anim)).element);
+    this.element.appendChild((new Button.StepForward(anim)).element);
+    this.element.appendChild((new Button.SkipToEnd(anim)).element);
   }
 }
 
