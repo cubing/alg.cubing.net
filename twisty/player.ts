d8d1e1d231aa897f4caf5226fb54f1f437afa7e9
Lucas Garron <code@garron.net>
2016-11-25 10:55:20 -0800

    Move auto-initialization code out of Twisty.Player.

--- twisty/player.ts
+++ twisty/player.ts
@@ -11,26 +11,26 @@ export class Player {
     this.element.appendChild((new Twisty.Widget.Scrubber(this.anim)).element);
     this.element.appendChild((new Twisty.Widget.ControlBar(this.anim, this.element)).element);
   }
+}
 
-  // Initialize a Twisty for the given Element unless the element's
-  // `initialization` attribute is set to `custom`.
-  private static autoInitialize(elem: Element) {
-    const ini = elem.getAttribute("initialization");
-    if (ini !== "custom") {
-      new Twisty.Player(elem);
-    }
-  }
-
-  static autoInitializePage() {
-    const elems = document.querySelectorAll("twisty");
-    console.log(`Found ${elems.length} twisty elem${elems.length === 1 ? "" : "s"} on page.`)
-
-    for (let i = 0; i < elems.length; i++) {
-      Twisty.Player.autoInitialize(elems[i]);
-    }
+// Initialize a Twisty for the given Element unless the element's
+// `initialization` attribute is set to `custom`.
+function autoInitialize(elem: Element) {
+  const ini = elem.getAttribute("initialization");
+  if (ini !== "custom") {
+    new Twisty.Player(elem);
   }
 }
 
-window.addEventListener("load", Twisty.Player.autoInitializePage);
+function autoInitializePage() {
+  const elems = document.querySelectorAll("twisty");
+  console.log(`Found ${elems.length} twisty elem${elems.length === 1 ? "" : "s"} on page.`)
+
+  for (let i = 0; i < elems.length; i++) {
+    autoInitialize(elems[i]);
+  }
+}
+
+window.addEventListener("load", autoInitializePage);
 
 }
