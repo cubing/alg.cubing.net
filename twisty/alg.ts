a7e4006e9cfebc40f1ee549d49f3e45377e6a2c2
Lucas Garron <code@garron.net>
2016-12-04 00:24:21 -0800

    Inversion

--- twisty/alg.ts
+++ twisty/alg.ts
@@ -95,11 +95,15 @@ export class BlockMove extends Repeatable implements NestableSegment {
   toString(): string {
     return this.base + this.repetitionSuffix();
   }
+  copy(): BlockMove {
+    // TODO: Take into account layers.
+    return new BlockMove(this.base, this.amount);
+  }
 }
 
 export class Commutator extends Repeatable implements NestableSegment {
   public type: string = "commutator";
-  constructor(public A: NestedSequence, public B: NestedSequence, amount: number) {
+  constructor(public A: NestableSegment, public B: NestableSegment, amount: number) {
     super(amount);
   }
   toString(): string {
@@ -109,7 +113,7 @@ export class Commutator extends Repeatable implements NestableSegment {
 
 export class Conjugate extends Repeatable implements NestableSegment {
   public type: string = "conjugate";
-  constructor(public A: NestedSequence, public B: NestedSequence, amount: number) {
+  constructor(public A: NestableSegment, public B: NestableSegment, amount: number) {
     super(amount);
   }
   toString(): string {
@@ -161,11 +165,89 @@ export class CommentLong extends NonRepeatable implements NestableSegment {
   }
 }
 
+// interface Transformation<Down, Up> {
+//   blockMove(blockMove: BlockMove, down: Down): Up
+//   group(group: Group, down: Down): Up
+// }
+
+export class Invert {
+  public static invert(segment: NestableSegment): NestableSegment;
+  public static invert(segment: Segment): Segment {
+    // TODO: Use a direct look up using e.g. hashmap instead of sequential if-else.
+         if (segment instanceof Sequence)       { return Invert.invertSequence(segment); }
+    else if (segment instanceof NestedSequence) { return Invert.invertNestedSequence(segment); }
+    else if (segment instanceof Group)          { return Invert.invertGroup(segment); }
+    else if (segment instanceof BlockMove)      { return Invert.invertBlockMove(segment); }
+    else if (segment instanceof Commutator)     { return Invert.invertCommutator(segment); }
+    else if (segment instanceof Conjugate)      { return Invert.invertConjugate(segment); }
+    else if (segment instanceof Pause)          { return Invert.invertPause(segment); }
+    else if (segment instanceof Newline)        { return Invert.invertNewline(segment); }
+    else if (segment instanceof CommentShort)   { return Invert.invertCommentShort(segment); }
+    else if (segment instanceof CommentLong)    { return Invert.invertCommentLong(segment); }
+    else {
+      throw "Unknown type of segment";
+    }
+  }
+
+  public static invertSequence(sequence: Sequence): Segment {
+    return new Sequence(sequence.segments.reverse().map(Invert.invert));
+  }
+  public static invertNestedSequence(nestedSequence: NestedSequence): NestableSegment {
+    return new NestedSequence(nestedSequence.segments.reverse().map(Invert.invert));
+  }
+  public static invertGroup(group: Group): NestableSegment {
+    return group; // TODO
+  }
+  public static invertBlockMove(blockMove: BlockMove): NestableSegment {
+    var b = blockMove.copy();
+    b.amount *= -1;
+    return b;
+  }
+  public static invertCommutator(commutator: Commutator): NestableSegment {
+    return new Commutator(
+      commutator.B,
+      commutator.A,
+      commutator.amount
+    );
+  }
+  public static invertConjugate(conjugate: Conjugate): NestableSegment {
+    return new Commutator(
+      conjugate.A,
+      Invert.invert(conjugate.B),
+      conjugate.amount
+    );
+  }
+  public static invertPause(pause: Pause): NestableSegment {
+    return pause; // TODO
+  }
+  public static invertNewline(newline: Newline): NestableSegment {
+    return newline; // TODO
+  }
+  public static invertCommentShort(commentShort: CommentShort): NestableSegment {
+    return commentShort; // TODO
+  }
+  public static invertCommentLong(commentLong: CommentLong): NestableSegment {
+    return commentLong; // TODO
+  }
+}
+
+
 // TODO
 // export class TimeStamp extends NonRepeatable implements Segment
 
 export namespace Example {
 export const Sune: Alg = new Alg([
+      // TODO: Use proper constructor instead of type assertion.
+      new BlockMove("R",  1),
+      new BlockMove("U",  1),
+      new BlockMove("R", -1),
+      new BlockMove("U",  1),
+      new BlockMove("R",  1),
+      new BlockMove("U",  2),
+      new BlockMove("R", -1)
+    ]);
+
+export const SuneSeq: Segment = new Sequence([
       // TODO: Use proper constructor instead of type assertion.
       new BlockMove("R",  1),
       new BlockMove("U",  1),
