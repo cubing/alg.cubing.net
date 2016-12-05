230651831d7f5dc7642fcfa55e026f151992b4fb
Lucas Garron <code@garron.net>
2016-12-04 22:05:13 -0800

    Recombine structure.js and part.js into algorithm.js .

--- twisty/alg/algorithm.ts
+++ twisty/alg/algorithm.ts
@@ -2,6 +2,31 @@
 
 namespace Alg {
 
+export abstract class Algorithm {
+  public readonly abstract type: string
+
+  clone():           Algorithm { return Alg.Traversal.Singleton.clone.traverse(this);           }
+  invert():          Algorithm { return Alg.Traversal.Singleton.invert.traverse(this);          }
+  expand():          Algorithm { return Alg.Traversal.Singleton.expand.traverse(this);          }
+  countBlockMoves(): number    { return Alg.Traversal.Singleton.countBlockMoves.traverse(this); }
+  coalesceMoves():   Algorithm { return Alg.Traversal.Singleton.coalesceMoves.traverse(this);   }
+  toString():        string    { return Alg.Traversal.Singleton.toString.traverse(this);        }
+
+  structureEquals(nestedAlg: Algorithm): boolean {
+    return Alg.Traversal.Singleton.structureEquals.traverse(this, nestedAlg);
+  }
+  concat(nestedAlg: Algorithm): Sequence {
+    return Alg.Traversal.Singleton.concat.traverse(this, nestedAlg);
+  }
+}
+
+export abstract class Repeatable extends Algorithm {
+  // TODO: Make `amount` an optional argument in derived class constructors.
+  constructor(public amount: number) {
+    super();
+  }
+}
+
 export type BaseMove = string; // TODO: Convert to an enum with string mappings.
 
 export class Sequence extends Algorithm {
