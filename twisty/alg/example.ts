a8229dde51d806096aedbc01b5c191421b0073e5
Lucas Garron <code@garron.net>
2016-12-04 17:14:47 -0800

    Fix indentation in Alg.Example.Sune .

--- twisty/alg/example.ts
+++ twisty/alg/example.ts
@@ -4,14 +4,14 @@ namespace Alg {
 export namespace Example {
 
 export const Sune: Sequence = new Sequence([
-      new BlockMove("R",  1),
-      new BlockMove("U",  1),
-      new BlockMove("R", -1),
-      new BlockMove("U",  1),
-      new BlockMove("R",  1),
-      new BlockMove("U",  2),
-      new BlockMove("R", -1)
-    ]);
+  new BlockMove("R",  1),
+  new BlockMove("U",  1),
+  new BlockMove("R", -1),
+  new BlockMove("U",  1),
+  new BlockMove("R",  1),
+  new BlockMove("U",  2),
+  new BlockMove("R", -1)
+]);
 
 export const FURURFCompact: Algorithm = new Conjugate(
   new BlockMove("F",  1),
