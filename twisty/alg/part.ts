8324edf50b6aae29e8a8a73b9b40624ae3991cd2
Lucas Garron <code@garron.net>
2016-12-04 22:01:37 -0800

    Condense Algorithm subclass constructors into a single line each.

--- twisty/alg/part.ts
+++ twisty/alg/part.ts
@@ -6,18 +6,14 @@ export type BaseMove = string; // TODO: Convert to an enum with string mappings.
 
 export class Sequence extends Algorithm {
   public type: string = "sequence";
-  constructor(public nestedAlgs: Algorithm[]) {
-    super();
-  }
+  constructor(public nestedAlgs: Algorithm[]) { super(); }
 }
 
 // Group is is like a Sequence, but is enclosed in parentheses when
 // written.
 export class Group extends Repeatable {
   public type: string = "group";
-  constructor(public nestedAlg: Algorithm, amount: number) {
-    super(amount);
-  }
+  constructor(public nestedAlg: Algorithm, amount: number) { super(amount); }
 }
 
 export class BlockMove extends Repeatable {
@@ -27,51 +23,37 @@ export class BlockMove extends Repeatable {
   public startLayer?: number;
   public endLayer?: number;
   // TODO: Handle layers in constructor
-  constructor(public base: BaseMove, amount: number) {
-    super(amount);
-  }
+  constructor(public base: BaseMove, amount: number) { super(amount); }
 }
 
 export class Commutator extends Repeatable {
   public type: string = "commutator";
-  constructor(public A: Algorithm, public B: Algorithm, amount: number) {
-    super(amount);
-  }
+  constructor(public A: Algorithm, public B: Algorithm, amount: number) { super(amount); }
 }
 
 export class Conjugate extends Repeatable {
   public type: string = "conjugate";
-  constructor(public A: Algorithm, public B: Algorithm, amount: number) {
-    super(amount);
-  }
+  constructor(public A: Algorithm, public B: Algorithm, amount: number) { super(amount); }
 }
 
 export class Pause extends Algorithm {
   public type: string = "pause";
-  constructor() {
-    super();
-  }
+  constructor() { super(); }
 }
 
 export class NewLine extends Algorithm {
   public type: string = "newLine";
-  constructor() {
-    super();
-  }
+  constructor() { super(); }
 }
 
 export class CommentShort extends Algorithm {
   public type: string = "commentShort";
-  constructor(public comment: string) {
-    super();
-  }
+  constructor(public comment: string) { super(); }
 }
 
 export class CommentLong extends Algorithm {
   public type: string = "commentLong";
-  constructor(public comment: string) {
-    super();
-  }
+  constructor(public comment: string) { super(); }
 }
 
 // TODO
