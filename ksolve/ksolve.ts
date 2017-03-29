c0ec206908e683179fc926efac6fd3b899ce3ff5
Lucas Garron <code@garron.net>
2017-03-29 01:55:20 -0700

    Implement KSolve SVG transitions.

--- ksolve/ksolve.ts
+++ ksolve/ksolve.ts
@@ -173,9 +173,13 @@ export class Puzzle {
   // invert(): this {}
 }
 
+var xmlns = "http://www.w3.org/2000/svg";
+
 export class SVG {
-  public element: HTMLElement; // TODO: SVGSVGElement?
+  public element: HTMLElement;
+  public gradientDefs: SVGDefsElement;
   private originalColors: {[type: string]: string} = {}
+  private gradients: {[type: string]: SVGGradientElement} = {}
   constructor(public puzzleDefinition: PuzzleDefinition) {
     if (!puzzleDefinition.svg) {
       throw `No SVG definition for puzzle type: ${puzzleDefinition.name}`
@@ -187,16 +191,51 @@ export class SVG {
     this.element.innerHTML = puzzleDefinition.svg;
     document.body.appendChild(this.element);
 
+    var svgElem = this.element.querySelector("svg");
+    if (xmlns !== svgElem.namespaceURI) {
+      throw "Unexpected XML namespace";
+    }
+    this.gradientDefs = <SVGDefsElement>document.createElementNS(xmlns, "defs");
+    svgElem.insertBefore(this.gradientDefs, svgElem.firstChild);
+
     for (var orbitName in puzzleDefinition.orbits) {
       var orbitDefinition = puzzleDefinition.orbits[orbitName];
 
       for (var idx = 0; idx < orbitDefinition.numPieces; idx++) {
         for (var orientation = 0; orientation < orbitDefinition.orientations; orientation++) {
           var id = this.elementID(orbitName, idx, orientation);
-          this.originalColors[id] = this.elementByID(id).style.fill as string;
+          var elem = this.elementByID(id);
+          var originalColor = elem.style.fill as string;
+          this.originalColors[id] = originalColor;
+          this.gradients[id] = this.newGradient(id, originalColor);
+          this.gradientDefs.appendChild(this.gradients[id]);
+          elem.setAttribute("style", `fill: url(#grad-${id})`);
         }
       }
     }
+  }
+
+  private newGradient(id: string, originalColor: string): SVGGradientElement {
+    var grad = <SVGGradientElement>document.createElementNS(xmlns, "radialGradient");
+    grad.setAttribute("id", `grad-${id}`);
+    grad.setAttribute("r", `70.7107%`); // TODO: Adapt to puzzle.
+    var stopDefs = [
+      {offset: 0, color: originalColor},
+      {offset: 0, color: originalColor},
+      {offset: 0, color: "black"},
+      {offset: 0, color: "black"},
+      {offset: 0, color: originalColor},
+      {offset: 100, color: originalColor}
+    ];
+    for (var stopDef of stopDefs) {
+      var stop = <SVGStopElement>document.createElementNS(xmlns,
+        "stop");
+      stop.setAttribute("offset", `${stopDef.offset}%`);
+      stop.setAttribute("stop-color", stopDef.color);
+      stop.setAttribute("stop-opacity", "1");
+      grad.appendChild(stop);
+    }
+    return grad;
   }
 
   private elementID(orbitName: string, idx: number, orientation: number): string {
@@ -212,19 +251,48 @@ export class SVG {
     for (var orbitName in definition.orbits) {
       var orbitDefinition = definition.orbits[orbitName];
 
-      var orbitState = state[orbitName];
-      if (fraction > 0.5) {
-        var orbitState = (nextState as Transformation)[orbitName];
-      }
+      var curOrbitState = state[orbitName];
+      var nextOrbitState = nextState ? (nextState as Transformation)[orbitName] : null;
       for (var idx = 0; idx < orbitDefinition.numPieces; idx++) {
         for (var orientation = 0; orientation < orbitDefinition.orientations; orientation++) {
           var id = this.elementID(orbitName, idx, orientation);
-          var from = this.elementID(
+          var fromCur = this.elementID(
             orbitName,
-            orbitState.permutation[idx] - 1,
-            (orbitDefinition.orientations - orbitState.orientation[idx] + orientation) % orbitDefinition.orientations
+            curOrbitState.permutation[idx] - 1,
+            (orbitDefinition.orientations - curOrbitState.orientation[idx] + orientation) % orbitDefinition.orientations
           );
-          this.elementByID(id).style.fill = this.originalColors[from];
+          var singleColor = false;
+          if (nextOrbitState) {
+            var fromNext = this.elementID(
+              orbitName,
+              nextOrbitState.permutation[idx] - 1,
+              (orbitDefinition.orientations - nextOrbitState.orientation[idx] + orientation) % orbitDefinition.orientations
+            );
+            if (fromCur === fromNext) {
+              singleColor = true; // TODO: Avoid redundant work during move.
+            }
+            var easedFraction = fraction * fraction * (2 - fraction * fraction); // TODO: Move easing up the stack.
+            this.gradients[id].children[0].setAttribute("stop-color", this.originalColors[fromNext]);
+            this.gradients[id].children[1].setAttribute("stop-color", this.originalColors[fromNext]);
+            this.gradients[id].children[1].setAttribute("offset", `${easedFraction*95}%`)
+            this.gradients[id].children[2].setAttribute("offset", `${easedFraction*95}%`)
+            this.gradients[id].children[3].setAttribute("offset", `${easedFraction*95+5}%`)
+            this.gradients[id].children[4].setAttribute("offset", `${easedFraction*95+5}%`)
+            this.gradients[id].children[4].setAttribute("stop-color", this.originalColors[fromCur]);
+            this.gradients[id].children[5].setAttribute("stop-color", this.originalColors[fromCur]);
+          } else {
+            singleColor = true; // TODO: Avoid redundant work during move.
+          }
+          if (singleColor) {
+            this.gradients[id].children[1].setAttribute("offset", `0%`)
+            this.gradients[id].children[2].setAttribute("offset", `0%`)
+            this.gradients[id].children[3].setAttribute("offset", `0%`)
+            this.gradients[id].children[4].setAttribute("offset", `0%`)
+            this.gradients[id].children[4].setAttribute("stop-color", this.originalColors[fromCur]);
+            this.gradients[id].children[5].setAttribute("stop-color", this.originalColors[fromCur]);
+          }
+          // this.gradients[id]
+          // this.elementByID(id).style.fill = this.originalColors[from];
         }
       }
     }
